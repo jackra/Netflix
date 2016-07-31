cdb36bbb76cd588750238c52bba313bf2dcda109
ganesh <ganeshr89@yahoo.co.in>
2016-07-31 01:03:10 -0400

    added exception classes

--- Netflix/src/main/java/com/egen/api/exception/MovieAlreadyExistException.java
+++ Netflix/src/main/java/com/egen/api/exception/MovieAlreadyExistException.java
@@ -3,7 +3,7 @@ package com.egen.api.exception;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
-@ResponseStatus(code = HttpStatus.BAD_REQUEST, reason = "User already exists")
+@ResponseStatus(code = HttpStatus.BAD_REQUEST, reason = "Movie already exists")
 public class MovieAlreadyExistException extends RuntimeException {
 	private static final long serialVersionUID = 479614122756850743L;
 
--- Netflix/src/main/java/com/egen/api/exception/MovieNotFound.java
+++ Netflix/src/main/java/com/egen/api/exception/MovieNotFound.java
@@ -3,7 +3,7 @@ package com.egen.api.exception;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
-@ResponseStatus(code = HttpStatus.NOT_FOUND, reason = "User not found")
+@ResponseStatus(code = HttpStatus.NOT_FOUND, reason = "Movie not found")
 public class MovieNotFound extends RuntimeException {
 
 	private static final long serialVersionUID = 479614122756850743L;
